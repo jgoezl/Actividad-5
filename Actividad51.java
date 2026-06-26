d9951bce4de54d9624a4ae7ca1e8c5b84a9e5a96
jgoezl <jgoezl@unal.edu.co>
2026-06-25 21:52:09 -0500

    Create Actividad51.java

---
+++ Actividad51.java
@@ -0,0 +1,11 @@
+package actividad5.pkg1;
+
+public class Actividad51 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+        
+    }
+    
+}
